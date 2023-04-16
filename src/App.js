539bffb079f0fbc7480c3a8f9a8d401806b5dd25
rajpreet-12 <rajpreet.singh3@triosstudent.com>
2023-04-16 03:53:32 -0400

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ><h1>hiiii</h1>
-        <p> </p>
-          Learn React
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
